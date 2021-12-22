bc47598d07bfe8d4985bc7050f61188fad4ca2d7
lusine <luskivi218@gmail.com>
2021-12-22 20:50:29 +0400

    added comments

--- homework_5/parallel_scheduler.cpp
+++ homework_5/parallel_scheduler.cpp
@@ -4,50 +4,70 @@ void* function(void* args_void)
 {
     while (true)
     {
+        // for errors
         int result;
 
         Args* args = (Args*) args_void;
         while (!(*(args->functions)).empty())
         {
+            // locks thread
             result = pthread_mutex_lock(*(args->mutex));    
+            
+            // handles error
             if (result != 0)
             {
                 std::cerr << "Error while locking" << std::endl;
                 exit(result);
             }
 
-
+            // function and argument
             auto current_func = (*(args->functions)).front();
             auto current_arg = (*(args->arguments)).front();
 
+            // removes function from queue
             (*(args->functions)).pop();
             (*(args->arguments)).pop();
             
+            // unlocks thread
             result = pthread_mutex_unlock(*(args->mutex));
+            
+            // handles error
             if (result != 0)
             {
                 std::cerr << "Error while unlocking" << std::endl;
                 exit(result);
             }
+
+            // runs function
             current_func(current_arg);
         }
         
         while ((*(args->functions)).empty())
         {
-            
+            // locks thread
             result = pthread_mutex_lock(*(args->mutex));
+            
+            // handles error
             if (result != 0)
             {
                 std::cerr << "Error while locking" << std::endl;
                 exit(result);
             }
+
+            // waits for signal
             result = pthread_cond_wait(*(args->cond), *(args->mutex));
+            
+            // handles error
             if (result != 0)
             {
                 std::cerr << "Error while start waiting" << std::endl;
                 exit(result);
             }
+
+            // unlocks thread
             result = pthread_mutex_unlock(*(args->mutex));
+
+            // handles error
             if (result != 0)
             {
                 std::cerr << "Error while unlocking" << std::endl;
@@ -60,22 +80,37 @@ void* function(void* args_void)
 
 void ParallelScheduler::run(void (*start_routine)(void*), void* arg)
 {
+    // for errors
     int result;
+
+    // locks thread
     result = pthread_mutex_lock(mutex);
+
+    // handles error
     if (result != 0)
     {
         std::cerr << "Error while locking" << std::endl;
         exit(result);
     }
+
+    // adds function to the queue
     functions.push(start_routine);
     arguments.push(arg);
+
+    // gives a signal
     result = pthread_cond_signal(cond);
+
+    // handles error
     if (result != 0)
     {
         std::cerr << "Error while signaling" << std::endl;
         exit(result);
     }
+
+    // locks thread
     result = pthread_mutex_unlock(mutex);
+
+    // handles error
     if (result != 0)
     {
         std::cerr << "Error while unlocking" << std::endl;
--- homework_5/parallel_scheduler.h
+++ homework_5/parallel_scheduler.h
@@ -4,7 +4,7 @@
 
 void* function(void* args_void);
 
-
+// structer for thread arguments
 struct Args
 {
     int* capacity;
@@ -13,6 +13,7 @@ struct Args
     pthread_mutex_t** mutex;
     pthread_cond_t** cond;
 
+    // constructer
     Args(int* capacity, std::queue<void(*)(void*)>* functions, std::queue<void*>* arguments, pthread_mutex_t** mutex, pthread_cond_t** cond)
         : capacity(capacity)
         , functions(functions)
@@ -22,52 +23,73 @@ struct Args
     {}
 };
 
+// module
 class ParallelScheduler
 {
 public:
+    // constructer
     ParallelScheduler(int capacity)
         : capacity(capacity)
         , threads(new pthread_t[capacity])
         , mutex(new pthread_mutex_t)
         , cond(new pthread_cond_t)
     {
+        // initializes mutex
         int mutex_init = pthread_mutex_init(mutex, NULL);
+        
+        // handles error
         if (mutex_init != 0)
         {
             std::cerr << "Error while initializing a mutex object" << std::endl;
 			exit(mutex_init);
         }
 
+        // initializes conditional variable
         int cond_init = pthread_cond_init(cond, NULL);
+        
+        // handles error
         if (cond_init != 0)
         {
             std::cerr << "Error while initializing a condition variable" << std::endl;
 			exit(cond_init);
         }
 
+        // creates arguments for threads
         Args* args = new Args(&capacity, &functions, &arguments, &mutex, &cond);
         void* args_void = (void*) args;
         for (int i = 0; i < capacity; ++i)
         {
+            // creates a thread
             int result = pthread_create(&threads[i], NULL, function, args_void);
+            
+            // handles error
             if (result != 0)
             {
                 std::cerr << "Error while creating thread" << std::endl;
                 exit(result);
             }
         }
+
+        // free allocated memory
+        delete args;
     }
 
     ~ParallelScheduler()
     {
+        // destroys conditional variable
         int cond_destroy = pthread_cond_destroy(cond);
+        
+        // handles error
         if (cond_destroy != 0)
         {
             std::cerr << "Error while destroying a conditional viriable state" << std::endl;
 			exit(cond_destroy);
         }
 
+        // destroys mutex
         int mutex_destroy = pthread_mutex_destroy(mutex);
+        
+        // handles error
         if (mutex_destroy != 0)
         {
             std::cerr << "Error while destroying a mutex object" << std::endl;
@@ -76,26 +98,34 @@ public:
         
         for (int i = 0; i < capacity; ++i)
         {
+            // joins thread
             int joined = pthread_join(threads[i], NULL);
 
+            // handles error
             if (joined != 0)
             {
                 std::cerr << "Error while joining thread" << std::endl;
                 exit(joined);
             }
         }
-
+        
+        // free allocated memory
         delete cond;
         delete mutex;
         delete [] threads;
     }
 
+    // calls funtions
     void run(void (*start_routine)(void*), void* arg);
 
 private:
     int capacity;
     pthread_t* threads;
+
+    // queue of functions
     std::queue<void(*)(void*)> functions;
+
+    // queue of arguments
     std::queue<void*> arguments;
     
     pthread_mutex_t* mutex;
