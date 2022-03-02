96aaf15ac5a86011d6d8a4f59626f22366de98ca
lusine <luskivi218@gmail.com>
2022-03-02 16:42:39 +0400

    comments added

--- homework_6/signal_handling.cpp
+++ homework_6/signal_handling.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <signal.h>
 #include <ucontext.h>
+#include <string.h>
+#include <unistd.h>
 
+// signal handler
 void signal_handler(int sig, siginfo_t * info, void *ucontext)
 {
+    if (sig != SIGUSR1)
+    {
+        return;
+    }
     std::cout << "PID: " << info->si_pid << std::endl;
     std::cout << "UID: " << info->si_uid << std::endl;
+    
+    // cast void* to ucontext_t*
     ucontext_t* context = (ucontext_t*)ucontext;
+    
+    // registers
     std::cout << "EIP: " << context->uc_mcontext.gregs[REG_RIP] << std::endl;
     std::cout << "EAX: " << context->uc_mcontext.gregs[REG_RAX] << std::endl;
     std::cout << "EBX: " << context->uc_mcontext.gregs[REG_RBX] << std::endl;
@@ -14,8 +25,18 @@ void signal_handler(int sig, siginfo_t * info, void *ucontext)
 
 int main()
 {
+    // create sigaction object
+    struct sigaction action;
+
+    // fill struct memory with 0
+    memset(&action, 0, sizeof(action));
+
+    // 
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = signal_handler;
     while (true)
     {
-        
+        sleep(10);
     }
+    return 0;
 }
