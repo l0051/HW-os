c90e32f067159d7dfd5b815eef05e7bfb2aa73e5
lusine <luskivi218@gmail.com>
2022-03-02 16:05:27 +0400

    signal handler started

---
+++ homework_6/signal_handling.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <signal.h>
+#include <ucontext.h>
+
+void signal_handler(int sig, siginfo_t * info, void *ucontext)
+{
+    std::cout << "PID: " << info->si_uid << std::endl;
+    std::cout << "UID: " << info->si_pid << std::endl;
+    ucontext_t* context = (ucontext_t*)ucontext;
+    std::cout << "EIP: " << context->uc_mcontext.gregs[REG_RIP] << std::endl;
+    std::cout << "EAX: " << context->uc_mcontext.gregs[REG_RAX] << std::endl;
+    std::cout << "EBX: " << context->uc_mcontext.gregs[REG_RBX] << std::endl;
+}
+
+int main()
+{
+    while (true)
+    {
+        
+    }
+}
