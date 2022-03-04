0a34d5c8aa2aa48a9442757ce1176724a93e0a08
lusine <luskivi218@gmail.com>
2022-03-05 01:53:41 +0400

    some coments added

--- homework_7/consumer.cpp
+++ homework_7/consumer.cpp
@@ -11,32 +11,40 @@ const size_t BUFFER_SIZE = getpagesize();
 
 int main()
 {
+    // (create and) open shared memory object
     int shared_fd = shm_open("/prod-cons-buffer", O_CREAT | O_RDONLY, S_IRGRP);
     
+    // handle error
     if (shared_fd < 0)
 	{
 		std::cerr << "Error " << std::endl;
 		exit(errno);
 	}
 
+    // change size of shared memory
     int er = ftruncate(shared_fd, getpagesize());
 
+    // handle error
     if (er < 0)
 	{
 		std::cerr << "Error " << std::endl;
 		exit(errno);
 	}
 
+    // creates a new mapping in the virtual address space of the calling process
     void* shared_pointer_void = mmap(NULL, BUFFER_SIZE, PROT_WRITE, PROT_WRITE, shared_fd, 0);
     
+    // handle error
     if (*(int *)shared_pointer_void < 0)
 	{
 		std::cerr << "Error " << std::endl;
 		exit(errno);
 	}
 
+    // semaphore for reading
     size_t* shared_fullness = (size_t*) shared_pointer_void;
     //*shared_fullness = 0;
+    
     char* shared_pointer = (char*) shared_pointer_void;
     shared_pointer += CHAR_BIT * sizeof(size_t);
 
@@ -46,6 +54,7 @@ int main()
 
     sem_t* sem_full = sem_open("/full", O_CREAT, 777, BUFFER_SIZE);
     
+    // handle error
     if (sem_full == SEM_FAILED)
 	{
 		std::cerr << "Error " << std::endl;
@@ -54,6 +63,7 @@ int main()
 
     sem_t* sem_empty = sem_open("/empty", O_CREAT, 777, 0);
 
+    // handle error
     if (sem_empty == SEM_FAILED)
 	{
 		std::cerr << "Error " << std::endl;
@@ -62,6 +72,7 @@ int main()
 
     er = sem_init(sem_shared, 0, 1);
 
+    // handle error
     if (er < 0)
 	{
 		std::cerr << "Error " << std::endl;
@@ -80,11 +91,20 @@ int main()
         sem_post(sem_full);
     }
 
+    // destroy unnamed semaphore
     sem_destroy(sem_shared);
+
+    // close named semaphore
     sem_close(sem_empty);
     sem_close(sem_full);
+
+    // free allocated memory
     delete sem_shared;
+
+    // removes the named semaphore referred to by name
     sem_unlink("/empty");
     sem_unlink("/full");
+
+    // remove an object previously created by shm_open()
     shm_unlink("/prod-cons-buffer");
 }
--- homework_7/producer.cpp
+++ homework_7/producer.cpp
@@ -11,38 +11,49 @@ const size_t BUFFER_SIZE = getpagesize();
 
 int main()
 {
+    // (create and) open shared memory object
     int shared_fd = shm_open("/prod-cons-buffer", O_CREAT | O_WRONLY, S_IWGRP);
 
+    // handle error
     if (shared_fd < 0)
 	{
 		std::cerr << "Error " << std::endl;
 		exit(errno);
 	}
 
+    // change size of shared memory
     int er = ftruncate(shared_fd, getpagesize());
 
+    // handle error
     if (er < 0)
 	{
 		std::cerr << "Error " << std::endl;
 		exit(errno);
 	}
     
+    // creates a new mapping in the virtual address space of the calling process
     void* shared_pointer_void = mmap(NULL, BUFFER_SIZE, PROT_WRITE, PROT_WRITE, shared_fd, 0);
 
+    // handle error
     if (*(int *)shared_pointer_void < 0)
 	{
 		std::cerr << "Error " << std::endl;
 		exit(errno);
 	}
 
+    // semaphore for writing
     size_t* shared_fullness = (size_t*) shared_pointer_void;
     //*shared_fullness = 0;
     char* shared_pointer = (char*) shared_pointer_void;
     shared_pointer += CHAR_BIT * sizeof(size_t);
+
+    mmap(NULL, BUFFER_SIZE, PROT_WRITE, PROT_WRITE, shared_fd, 0);
+
     sem_t* sem_shared = new sem_t;
 
     sem_t* sem_full = sem_open("/full", O_CREAT, 777, BUFFER_SIZE);
 
+    // handle error
     if (sem_full == SEM_FAILED)
 	{
 		std::cerr << "Error " << std::endl;
@@ -51,6 +62,7 @@ int main()
     
     sem_t* sem_empty = sem_open("/empty", O_CREAT, 777, 0);
     
+    // handle error
     if (sem_empty == SEM_FAILED)
 	{
 		std::cerr << "Error " << std::endl;
@@ -59,6 +71,7 @@ int main()
 
     er = sem_init(sem_shared, 0, 1);
 
+    // handle error
     if (er < 0)
 	{
 		std::cerr << "Error " << std::endl;
@@ -76,11 +89,20 @@ int main()
         sem_post(sem_empty);
     }
     
+    // destroy unnamed semaphore
     sem_destroy(sem_shared);
+
+    // close named semaphore
     sem_close(sem_empty);
     sem_close(sem_full);
+
+    // free allocated memory
     delete sem_shared;
+
+    // removes the named semaphore referred to by name
     sem_unlink("/empty");
     sem_unlink("/full");
+
+    // remove an object previously created by shm_open()
     shm_unlink("/prod-cons-buffer");
 }
