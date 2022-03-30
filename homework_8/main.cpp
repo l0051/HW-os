c47b9fb86d5df63324fd4328f54baea901071963
lusine <luskivi218@gmail.com>
2022-03-30 21:04:30 +0400

    array created

--- homework_8/main.cpp
+++ homework_8/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    // make array with random numbers
+    size_t n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    srand(time(NULL));
+    for (size_t i = 0; i < n; ++i)
+    {
+        v[i] = rand() % 10000;
+    }
+
+    // get number of paralell workers
+    size_t m;
+    std::cin >> m;
+
     
 }
