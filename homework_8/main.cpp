adeb56555b30e2eaa20d6620f437aa3c9727f45c
lusine <luskivi218@gmail.com>
2022-03-31 12:07:38 +0400

    added fork

--- homework_8/main.cpp
+++ homework_8/main.cpp
@@ -2,22 +2,64 @@
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <cerrno>
 
 int main()
 {
     // make array with random numbers
     size_t n;
     std::cin >> n;
-    std::vector<int> v(n);
+    int* array = new int[n];
     srand(time(NULL));
     for (size_t i = 0; i < n; ++i)
     {
-        v[i] = rand() % 10000;
+        array[i] = rand() % 10000;
     }
 
     // get number of paralell workers
     size_t m;
     std::cin >> m;
 
-    
+    for (size_t i = 0; i < m; ++i)
+    {
+        int pipefd_index[2];
+        int pipefd_sum[2];
+        int res_index = pipe(pipefd_index);
+        int res_sum = pipe(pipefd_sum);
+
+        // error handling
+        if (res_index < 0)
+        {
+            std::cerr << "Error while creating pipe " << errno <<std::endl;
+            exit(errno);
+        }
+        if (res_sum < 0)
+        {
+            std::cerr << "Error while creating pipe " << errno <<std::endl;
+            exit(errno);
+        }
+        
+        // clone the process
+        pid_t pid = fork();
+
+        // error handling
+        if (pid < 0)
+        {
+            std::cerr << "Error while making fork " << errno <<std::endl;
+            exit(errno);
+        }
+
+        // child process
+        if (pid == 0)
+        {
+
+        }
+
+        // parent process
+        if (pid > 0)
+        {
+            
+        }
+    }
 }
