ff9f36cd22752d4e14f98c27547baab687829661
lusine <luskivi218@gmail.com>
2022-05-05 23:20:45 +0400

    server_socket created

--- homework_9/lib/http.cpp
+++ homework_9/lib/http.cpp
@@ -1,1 +1,40 @@
 #include "http.h"
+
+//Http::Response() = default;
+
+//Http::Request() = default;
+
+Request get_request(int socket_fd) // arg?
+{
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in address;
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port); // port?
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int bound = bind(server_fd, (const struct sockaddr*) &address, size_of(address));
+
+    int listening = listen(socket_fd, NUM_OF_THREADS);
+
+    //thread pool ..
+}
+
+Response produce_response(const Request& request)
+{
+
+}
+
+void send_response(const Response& response, int socket_fd)
+{
+
+}
+
+void _server_(int socket_fd)
+{
+    while (true)
+    {
+        get_request(socket_fd);
+    }
+}
--- homework_9/lib/http.h
+++ homework_9/lib/http.h
@@ -1,55 +1,51 @@
 #ifndef HOMEWORK_9_HTTP_H
 #define HOMEWORK_9_HTTP_H
 
+
 #include <iostream>
 #include <string>
 #include <map>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
- /*
-  * ???????????????????????
-  * The Http class may be extra
-  * Adding reason : client could use only one function
-  */
+#define NUM_OF_THREADS 1000
 
-class Http
+class Response
 {
-private:
-    class Response
-    {
-    public:
-        Response();
-
-    private:
-        std::string method;
-        std::string path;
-        std::string body;
-        std::map<std::string, std::string> headers;
-    };
-
-    class Request
-    {
-    public:
-        Request();
-
-    private:
-        std::string method;
-        std::string path;
-        std::map<std::string, std::string> headers;
-    };
-
-    // read request from the socket
-    Request get_request(int socket_fd);
-
-    // produce response for given request
-    Response produce_response(const Request& request);
-
-    // send response to the client
-    void send_response(const Response& response, int socket_fd);
-
 public:
-    /* name and return value of function ?? */
-    // main function for library users
-    void _server_(int socket_fd);
+    Response();
 
+private:
+    std::string method;
+    std::string path;
+    std::string body;
+    std::map<std::string, std::string> headers;
 };
+
+class Request
+{
+public:
+    //Request();
+
+private:
+    std::string method;
+    std::string path;
+    std::map<std::string, std::string> headers;
+};
+
+// read request from the socket
+Request get_request(int socket_fd);
+
+// produce response for given request
+Response produce_response(const Request& request);
+
+// send response to the client
+void send_response(const Response& response, int socket_fd);
+
+/* name and return value of function ?? */
+// main function for library users
+void _server_(int socket_fd);
+
+
 #endif //HOMEWORK_9_HTTP_H
